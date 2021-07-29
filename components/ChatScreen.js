be23a2268fb76c6f8a6d7bc16b144b03fb6db377
Ziyaad Lakay <ZiyaadL@reutech.co.za>
2021-07-29 16:25:09 +0200

    chat header

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,8 +1,30 @@
+import { Avatar } from "@material-ui/core";
+import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
-function ChatScreen() {
-  return <Container></Container>;
+import { auth } from "../firebase";
+
+function ChatScreen({ chat, messages }) {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  return (
+    <Container>
+      <Header>
+        <Avatar />
+
+        <HeaderInformation>
+          <h3>Rec Email</h3>
+          <p>Last Seen ...</p>
+        </HeaderInformation>
+      </Header>
+    </Container>
+  );
 }
 
 export default ChatScreen;
 
 const Container = styled.div``;
+
+const Header = styled.div``;
+
+const HeaderInformation = styled.div``;
--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,17 +1,22 @@
 import Head from "next/head";
+import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
 import ChatScreen from "../../components/ChatScreen";
 import Sidebar from "../../components/Sidebar";
-function Chat() {
+import { auth, db } from "../../firebase";
+import getRecipientEmail from "../../utils/getRecipientEmail";
+
+function Chat({ chat, messages }) {
+  const [user] = useAuthState(auth);
   return (
     <Container>
       <Head>
-        <title>Chat</title>
+        <title>Chat with {getRecipientEmail(chat.users, user)}</title>
       </Head>
       <Sidebar />
 
       <ChatContainer>
-        <ChatScreen />
+        <ChatScreen chat={chat} messages={messages} />
       </ChatContainer>
     </Container>
   );
@@ -24,7 +29,7 @@ export async function getServerSideProps(context) {
 
   const messagesRes = await ref
     .collection("messages")
-    .order("timestamp", asc)
+    .orderBy("timestamp", "asc")
     .get();
 
   const messages = messagesRes.docs
@@ -36,6 +41,21 @@ export async function getServerSideProps(context) {
       ...messages,
       timestamp: messages.timestamp.toDate().getTime(),
     }));
+
+  // PREP the chats
+
+  const chatRes = await ref.get();
+  const chat = {
+    id: chatRes.id,
+    ...chatRes.data(),
+  };
+
+  return {
+    props: {
+      messages: JSON.stringify(messages),
+      chat: chat,
+    },
+  };
 }
 
 const Container = styled.div`
